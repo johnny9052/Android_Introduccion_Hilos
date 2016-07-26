42d96337f1238ffd04b82895f6df3e9e5c72ab4c
johnny9052 <alexander9052@gmail.com>
2016-07-26 11:32:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/johnny/android_introduccion_hilos/MainActivity.java
@@ -0,0 +1,181 @@
+package com.example.johnny.android_introduccion_hilos;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+
+/***********************Teoria************************/
+
+/*
+* Todos los componentes de una aplicación Android, tanto las actividades, los servicios, la interfaz
+ * grafica se ejecutan en el mismo hilo de ejecución, el llamado hilo principal, main thread o GUI
+ * thread. Es por ello, que cualquier operación larga o costosa que realicemos en este hilo va a
+ * bloquear la ejecución del resto de componentes de la aplicación y por supuesto también la
+ * interfaz, produciendo al usuario un efecto evidente de bloqueo. Incluso puede ser peor, dado que
+  * Android monitoriza las operaciones realizadas en el hilo principal y detecta aquellas que
+  * superen los 5 segundos, en cuyo caso se muestra el famoso mensaje de “Application Not
+  * Responding” (ANR) y el usuario debe decidir entre forzar el cierre de la aplicación o esperar a
+  * que termine.
+* */
+
+
+/**
+ * Como utilizar el primer boton: Si solo se presiona el boton y se deja la aplicacion quieta, puede
+ * durar entre 10 y 12 segundos, pero si se presiona la pantalla mientras el proceso ocurre ese tiempo
+ * aumenta drasticamente o hasta se bloquea el app
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar pbarProgreso; //Referencia para barra de progreso
+    clsAsincrono tarea;  //Referencia para clase asincrona
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*Se referencia la barra de progreso*/
+        pbarProgreso = (ProgressBar)findViewById(R.id.progressBar);
+        /*Se instancia la clase asincrona, mandando como parametro la clase que la llama, como
+        * tambien la barra de progreso que sera modificada desde el hilo*/
+    }
+
+
+    /*Reinicia la barra de progreso en 0 */
+    public void limpiar(View view){
+        pbarProgreso.setProgress(0);
+    }
+
+    /*Detenemos el hilo principal durante 1 segundo*/
+    private void detenerHilo()
+    {
+        try {
+            Thread.sleep(1000);
+        } catch(InterruptedException e) {
+            //Si sucede error se le indica al usuario
+        }
+    }
+
+
+    /**************USO DEL HILO PRINCIPAL***********************************/
+    /*******************Oppcion no recomendada, ya que bloquea la app*******/
+
+    public void clickUsoHiloPrincipal(View view){
+        /*Se establece el progress bar en 100 como maximo*/
+        pbarProgreso.setMax(100);
+        /*Se establece su posicion actual en 0*/
+        pbarProgreso.setProgress(0);
+
+        for(int i=1; i<=10; i++) {
+            /*Se detiene el hilo 1 segundo*/
+            detenerHilo();
+            /*Aumenta su progreso */
+            pbarProgreso.incrementProgressBy(10);
+        }
+
+        Toast.makeText(MainActivity.this, "Tarea finalizada!",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    /**************END USO DEL HILO PRINCIPAL***********************************/
+
+
+
+    /**************CREACION DE UN HILO BASICO***********************************/
+    /**Oppcion no recomendada, ya que es dificil de mantener, realizar modificaciones
+     * y cuando es mucho codigo es complejo de entender *******/
+
+
+    /*Este hilo por si mismo no tiene acceso a los componentes del hilo principal, como la interfaz
+    * grafica, para acceder a estos recursos se puede utilizar el metodo post. Tambien esta el
+    * metodo runOnUiThread() para enviar operaciones al hilo principal*/
+
+    public void clickHiloSinAccesoAOtrosRecursos(View view){
+        new Thread(new Runnable() {
+            public void run() {
+
+                /*Con el metodo post accedemos al progress bar*/
+                pbarProgreso.post(new Runnable() {
+                    public void run() {
+                        pbarProgreso.setProgress(0);
+                    }
+                });
+
+                /*Realizamos el proceso*/
+                for(int i=1; i<=10; i++) {
+                    detenerHilo();
+                    /*Con el metodo post accedemos al progress bar*/
+                    pbarProgreso.post(new Runnable() {
+                        public void run() {
+                            pbarProgreso.incrementProgressBy(10);
+                        }
+                    });
+                }
+
+                /*Con el metodo runOnUiThread mostramos el toast en el hilo principal*/
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Tarea finalizada!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).start();
+    }
+
+
+
+
+    /************** USO DE LA CLASE ASYNCTASK, OPCION RECOMENDADA*********************************/
+
+    /* la clase AsyncTask, permiti realizar lo del boton anterior pero con la ventaja de no tener
+    que utilizar artefactos del tipo runOnUiThread() y de una forma mucho más organizada y legible.
+    La forma básica de utilizar la clase AsyncTaskconsiste en crear una nueva clase que extienda de
+    ella y sobrescribir varios de sus métodos entre los que repartiremos la funcionalidad de
+    nuestra tarea.
+    * */
+
+    /*Se inicia el hilo*/
+    public void clickClaseAsincrona (View view){
+        tarea = new clsAsincrono(MainActivity.this,pbarProgreso);
+        tarea.execute();
+    }
+
+    /*Se detiene el hilo*/
+    public void clickCancelarClaseAsincrona (View view){
+        tarea.cancel(true);
+    }
+
+    /**************END USO DE LA CLASE ASYNCTASK, OPCION RECOMENDADA*****************************/
+
+
+
+    public void clickClaseAsincronaVentana (View view){
+        /*Se define un progressDialog*/
+        ProgressDialog pDialog = new ProgressDialog(MainActivity.this);
+        /*Se define de estilo horizontal o el que se desee*/
+        pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        /*Mensaje del dialog*/
+        pDialog.setMessage("Procesando...");
+        /*Se determina si la ventana puede ser cerrada por el usuario*/
+        pDialog.setCancelable(false);
+        /*Valor maximo del proceso*/
+        pDialog.setMax(100);
+        /*Se muestra el dialog*/
+        pDialog.show();
+        /*Se define una instancia de la clase*/
+        clsAsincronoVentana proceso = new clsAsincronoVentana(MainActivity.this,pDialog);
+        /*Se ejecuta el hilo*/
+        proceso.execute();
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/johnny/android_introduccion_hilos/clsAsincronoVentana.java
@@ -0,0 +1,124 @@
+package com.example.johnny.android_introduccion_hilos;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+/**
+ * Created by Johnny on 21/06/2016.
+ */
+
+/*
+onPreExecute(). Se ejecutará antes del código principal de nuestra tarea. Se suele utilizar para
+                preparar la ejecución de la tarea, inicializar la interfaz, etc.
+doInBackground(). Contendrá el código principal de nuestra tarea.
+onProgressUpdate(). Se ejecutará cada vez que llamemos al método publishProgress() desde el método
+                    doInBackground().
+onPostExecute(). Se ejecutará cuando finalice nuestra tarea, o dicho de otra forma, tras la
+                 finalización del método doInBackground().
+onCancelled(). Se ejecutará cuando se cancele la ejecución de la tarea antes de su finalización
+               normal.
+*/
+
+
+/*Los parametros que estamos recibiendo al heredar del AsyncTask son:
+
+      Parametro 1: El tipo de datos que recibiremos como entrada de la tarea en el método
+                   doInBackground(). En este caso doInBackground() no recibirá ningún parámetro
+                   de entrada (Void).
+      Parametro 2: El tipo de datos con el que actualizaremos el progreso de la tarea, y que
+                   recibiremos como parámetro del método onProgressUpdate() y que a su vez
+                   tendremos que incluir como parámetro del método publishProgress(). En este
+                   caso publishProgress() y onProgressUpdate() recibirán como parámetros datos de
+                   tipo entero (Integer).
+      Parametro 3: El tipo de datos que devolveremos como resultado de nuestra tarea, que será el
+                   tipo de retorno del método doInBackground() y el tipo del parámetro recibido en
+                   el método onPostExecute(). En este caso doInBackground() devolverá como retorno
+                   un dato de tipo booleano y onPostExecute() también recibirá como parámetro un
+                   dato del dicho tipo (Boolean).
+*/
+public class clsAsincronoVentana extends AsyncTask<Void, Integer, Boolean> {
+
+    /*SE MODIFICO EL TIPO DE DATO A PROGRESSDIALOG*/
+    ProgressDialog pDialog;
+    private Activity activity;
+
+    public clsAsincronoVentana(Activity activity, ProgressDialog pDialog) {
+        /*SE MODIFICO EL CONSTRUCTOR PARA QUE SOPORTARA EL PROGRESSDIALOG*/
+        this.activity = activity;
+        this.pDialog = pDialog;
+    }
+
+
+    private void detenerHilo()
+    {
+        try {
+            Thread.sleep(1000);
+        } catch(InterruptedException e) {}
+    }
+
+
+    /*Antes de iniciar el proceso, reiniciamos la barra de progreso*/
+    @Override
+    protected void onPreExecute() {
+        pDialog.setMax(100);
+        pDialog.setProgress(0);
+    }
+
+
+    /*
+    * El método doInBackground() se ejecuta en un hilo secundario (por tanto no podremos interactuar
+    * con la interfaz), pero sin embargo todos los demás se ejecutan en el hilo principal, lo que
+    * quiere decir que dentro de ellos podremos hacer referencia directa a nuestros controles de
+    * usuario para actualizar la interfaz. Ademas si se llama al metodo publishProgress() este
+    * automáticamente ejecuta el onProgressUpdate() que actualizara la interfaz si es necesario
+    * */
+    @Override
+    protected Boolean doInBackground(Void... params) {
+
+        for(int i=1; i<=10; i++) {
+            detenerHilo();
+            /*Se llama a la funcion onProgressUpdate para actualizar la interfaz*/
+            publishProgress(i*10);
+            /*Se valida si se ha solicitado finalizar el proceso*/
+            if(isCancelled())
+                /*Se llama a la funcion onCancelled*/
+                break;
+        }
+
+        /*Se le retorna true a la funcion onPostExecute*/
+        return true;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        /*Se saca el progreso mandado y se actualiza en la barra de progreso */
+        int progreso = values[0].intValue();
+        /*SE MODIFICO PARA QUE SE ACTUALIZARA EL PROGRESSDIALOG*/
+        pDialog.setProgress(progreso);
+    }
+
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        /*Tan pronto termine el proceso, se muestra un toast en la activity indincando que termino
+        * el proceso*/
+        if(result)
+
+            /*SE ADICIONO QUE CERRARA EL PROGRESSDIALOG*/
+            pDialog.dismiss();
+
+            Toast.makeText(activity, "Tarea finalizada desde la clase asincrona!",
+                    Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCancelled() {
+        /*Si se cancela el proceso se le indica al usuario*/
+        Toast.makeText(activity, "Tarea cancelada!",
+                Toast.LENGTH_SHORT).show();
+    }
+
+}
